971bb912b6eb2a3d8848eb8874890ca900526683
monterosergio001 <sergiomontero001.alu@stemgranada.com>
2025-11-25 10:52:49 +0100

    proceso importar imagenes

--- defensorpolonorte/src/App.jsx
+++ defensorpolonorte/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { useReducer } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <h1 className='col-12'></h1>
+          <button className='col-5' onClick={() => dispatch({ type: 'CLICK_DISPARAR' })}>
+            <img className='img-fluid' src={} />
+          </button>
+        </div>
+        <div className='row justify-content-center'>
+          <button className='col-md-2 col-12' onClick={() => dispatch({ type: 'BUY_CANION' })}>
+            <img className='img-fluid' src={} />
+            x{state.cursorCount}
+          </button>
+          <button className='col-md-2 col-12' onClick={() => dispatch({ type: 'BUY_LANZAMISILES' })}>
+            <img className='img-fluid' src={} />
+            x{state.clickMultiplier}
+          </button>
+          <button className='col-md-2 col-12' onClick={() => dispatch({ type: 'BUY_ARBOL' })}>
+            <img className='img-fluid' src={} />
+            x{state.grandmaCount}
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
